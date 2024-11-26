6258042967647afd960a8724b3a8469f816a5221
6hislain <ghislainmike80@gmail.com>
2024-11-26 17:43:05 +0200

    eloquent

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,28 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
 
-class Job
+class Job extends Model
 {
-    public static function all(): array
-    {
-        return  [
-            [
-                "id" => 1,
-                "title" => 'Teacher',
-                "salary" => '40,000'
-            ],
-            [
-                "id" => 2,
-                "title" => 'Preacher',
-                "salary" => '60,000'
-            ],
-        ];
-    }
-
-    public static function find(int $id): array|null
-    {
-        return Arr::first(static::all(), fn($job) => $job['id'] == $id);
-    }
+    protected $table = 'job_listings';
+    protected $fillable = ['title', 'salary'];
 }
